5936e678f107b7418359e5465cd8902872d7b6c5
Muhammed Muflih <24mcam05@meaec.edu.in>
2025-09-16 11:12:48 +0530

    hfgj

---
+++ string.py
@@ -0,0 +1,8 @@
+input_string=input("enter a string:")
+n=int(input("eneter the number of copies(non negative integers):"))
+if n<0:
+    print("error:number of copies must be integer")
+else:
+    result=input_string*n
+    print(f"result:{result}")
+
