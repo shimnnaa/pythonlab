e2d07bd5165b8a157c6b236061e3b8ab48254e2a
shimnnaa <shimnafathima8@gmail.com>
2025-09-17 12:36:12 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,7 +1,7 @@
 input_string=input("enter a string:")
 n=int(input("eneter the number of copies(non negative integers):"))
 if n<0:
-    print("error:number of copies must be integer")
+    print("error:number of copies must be inteeger")
 else:
     result=input_string*n
     print(f"result:{result}")
