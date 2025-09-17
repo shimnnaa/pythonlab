bdeae5c22cfbe64d157764df51ee76a43711e258
shimnnaa <shimnafathima8@gmail.com>
2025-09-17 12:35:51 +0530

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,5 +1,4 @@
-hguwegyfgwfwfw
-wergfwg
-wg
-gw
-e
+import math
+radius=float(input("enter the radius of the circle"))
+area=math.pi*radius**2
+print(f"the area of the circleof the radius {radius} is {area:.2f}")
